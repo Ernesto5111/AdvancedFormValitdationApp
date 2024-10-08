d496b57d7d2a906e84519cc8dd8a07ee3b5ef94e
Ernesto5111 <162698713+Ernesto5111@users.noreply.github.com>
2024-10-08 11:07:02 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/advancedformvalitdationapp/MainActivity.java
+++ app/src/main/java/com/example/advancedformvalitdationapp/MainActivity.java
@@ -37,22 +37,44 @@ public class MainActivity extends AppCompatActivity {
 
         Name = findViewById(R.id.Name);
         Surname = findViewById(R.id.Surname);
+        Phone = findViewById(R.id.Phone);
+        Email = findViewById(R.id.Email);
+        Password = findViewById(R.id.Passowrd);
+        Password2 = findViewById(R.id.Passowrd2);
         button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 String name = Name.getText().toString().trim();
-                String email = Surname.getText().toString().trim();
+                String surname = Surname.getText().toString().trim();
+                String phone = Phone.getText().toString().trim();
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                String password2 = Password2.getText().toString().trim();
+
+
 
                 if(name.isEmpty()){
                     Toast.makeText(MainActivity.this, "Proszę wpisać imię",Toast.LENGTH_SHORT).show();
+                }
+                else if(surname.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Proszę wpisać Nazwisko",Toast.LENGTH_SHORT).show();
+                }
+                else if(phone.length() < 9 ){
+                    Toast.makeText(MainActivity.this, "Niepoprawny Numer tel.",Toast.LENGTH_SHORT).show();
                 }
                 else if(email.isEmpty()){
                     Toast.makeText(MainActivity.this, "Proszę wpisać email",Toast.LENGTH_SHORT).show();
                 }
                 else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
                     Toast.makeText(MainActivity.this, "Niepoprawny adres email",Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length() < 6){
+                    Toast.makeText(MainActivity.this, "Hasło jest za krótkie",Toast.LENGTH_SHORT).show();
+                }
+                else if(!password2.equals(password)){
+                    Toast.makeText(MainActivity.this, "Powtórz hasło",Toast.LENGTH_SHORT).show();
                 }
                 else{
                     Toast.makeText(MainActivity.this, "Formularz przesłany poprawnie",Toast.LENGTH_SHORT).show();
